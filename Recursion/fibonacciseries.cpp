f95f7a93408add8ee2fbfafaf095c061a9be38c3
pratik8696 <mepratikanand@gmail.com>
2021-08-16 13:24:31 +0530

    .....

--- Recursion/fibonacciseries.cpp
+++ Recursion/fibonacciseries.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int fun(int n)
+int fib(int n)
 {
-    if(n==0)
+    if(n<=1)
+    return n;
+
+    else
+    {
+        return fib(n-1)+fib(n-2);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fib(n);
 }
