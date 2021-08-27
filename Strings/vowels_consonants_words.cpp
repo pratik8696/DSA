4d020eb3a7a59efb67eaabdda6a1bbd50639d8e2
pratik8696 <mepratikanand@gmail.com>
2021-08-27 08:12:12 +0530

    done

--- Strings/vowels_consonants_words.cpp
+++ Strings/vowels_consonants_words.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
 using namespace std;
+static int words = 1;
+static int consonants;
+static int vowels;
 
-int vowels(string sentence)
+void check(string sentence)
 {
-    for (int i = 0; i < count; i++)
+
+    for (int i = 0; sentence[i] != '\0'; i++)
     {
+        if(i==0&&sentence[i]==' ')
+        {
+            continue;
+        }
+        else
+        if (sentence[i] == 'A' || sentence[i] == 'E' || sentence[i] == 'I' || sentence[i] == 'O' || sentence[i] == 'U' || sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u')
+        {
+            vowels++;
+        }
         
+        else if (sentence[i] == ' ' && (sentence[i - 1] != ' '))
+        {
+            words++;
+        }
+
+        else
+        {
+            if(sentence[i]==' ')
+            {continue;}
+            consonants++;
+        }
     }
-    
+}
+int largestword(string sentence)
+{
 }
 int main()
 {
     string sentence;
-    getline(cin,sentence);
-    vowels(sentence);
+    getline(cin, sentence);
+    check(sentence);
+    cout << "The no of words in " << sentence << " are = " << words << endl;
+    cout << "The no of vowels in " << sentence << " are = " << vowels << endl;
+    cout << "The no of consonants in " << sentence << " are = " << consonants << endl;
     return 0;
 }
