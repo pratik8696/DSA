a9f9752034d97433193e9abe6ae70f4b2c008ce3
pratik8696 <mepratikanand@gmail.com>
2021-09-05 14:41:10 +0530

    linked list initiated

--- LinkedList/printing.cpp
+++ LinkedList/printing.cpp
@@ -6,10 +6,44 @@ struct Node
     int data;
     struct Node *next;
 };
+struct Node *first = NULL;
+void Create(int arr[],int size)
+{
+    first=new Node;
+    struct Node *last,*t;
+    first->data=arr[0];
+    first->next=NULL;
+    last=first;
+
+       for (int i = 1; i < size; i++)
+       {
+           t=new Node;
+           t->data=arr[i];
+           t->next=NULL;
+           last->next=t;
+           last=t;
+       }
+            
+}
+
+
+int Display(struct Node *p)
+{
+    if(p!=NULL)
+    {
+        cout<<p->data<<" ";
+        Display(p->next);
+    }
+    else if(p==NULL){
+        return 0;
+    }
+}
 
 int main()
 {
-    struct Node a;
-    
+    int array[10]={1,2,3,4,5,6,7,8,9,10};
+    // constructing a linked list
+    Create(array,10);
+    Display(first);
     return 0;
 }
