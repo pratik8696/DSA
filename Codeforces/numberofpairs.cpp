65b923f0e740836d2ff8ae691d11df09290ce24f
pratik8696 <mepratikanand@gmail.com>
2021-09-17 07:49:51 +0530

    solved one problem

---
+++ Codeforces/numberofpairs.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,l,r,count=0;
+    cin>>n>>l>>r;
+    int arr[n];
+    int hash_size=10000;
+    int a[hash_size];
+    // zeroes in hash table
+    for (int i = 0; i < hash_size; i++)
+    {
+        a[i]=0;
+    }
+    // taking in input
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+        a[arr[i]]++;
+        int x = l - i;
+        int y = r - i;
+
+        for (; x <= y; x++)
+        {
+            if (a[x] != 0)
+            {
+                count++;
+            }
+        }
+    }
+    cout<<count<<endl;
+    for (int i = 0; i < hash_size; i++)
+    {
+        cout<<a[i]<<" ";
+    }   
+}
+
+    // finding the pairs
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int x=l-i;
+    //     int y=r-i;
+
+    //     for(;x<=y;x++)
+    //     {
+    //         if(a[x]!=0)
+    //         {
+    //             count++;
+    //         }
+    //     }
+    // }
