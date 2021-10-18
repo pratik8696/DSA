aeab9134066b5c9a025a10f119a14771608d2d4f
pratik8696 <mepratikanand@gmail.com>
2021-10-19 00:03:05 +0530

    array subarray interview questions

---
+++ 13. Deque/Array_Subarray.cpp
@@ -0,0 +1,105 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 1000000007;
+double eps = 1e-12;
+#define forn(i, n) for (ll i = 0; i < n; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln cout<<endl
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    deque<int> d;
+    int n, k, i = 0;
+    cin >> n >> k;
+    int arr[n];
+    forn(i, n)
+    {
+        cin >> arr[i];
+    }
+    // now processing for first k elements
+    for (i = 0; i < k; i++)
+    {
+        while (!(d.empty()) && arr[i] > arr[d.back()])
+        {
+            d.pop_back();
+        }
+        d.push_back(i);
+    }
+    // now processing further for remaining elements
+    for (; i < n; i++)
+    {
+        cout << arr[d.front()] << " ";
+        // remove the element which are not part of the window
+        if ((!d.empty()) && ((d.front()) <= (i - k)))
+        {
+            d.pop_front();
+        }
+        // remove the element which are of no use
+        if (!d.empty()&&arr[i]>arr[d.back()])
+        {
+            d.pop_back();
+        }
+        // add the remaining elements
+        d.push_back(i);
+    }
+    cout<<arr[d.front()]<<endl;
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for (int it = 1; it <= t; it++)
+    {
+        solve();
+    }
+    return 0;
+}
